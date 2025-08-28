8ccc53b20441a32dab14e782508f3b62cdd869ae
elferchando <efpa1998@hotmail.com>
2025-08-28 17:59:05 -0500

    updated package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // astro.config.mjs
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
 
 export default defineConfig({
   integrations: [react(), tailwind()],
+  output: "server",
+  adapter: node({ mode: "standalone" }),
+  server: { host: "0.0.0.0" } 
 });
