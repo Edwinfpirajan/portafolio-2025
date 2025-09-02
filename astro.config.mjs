5fefdc2c5a8926d0f7ab6970a87ae4763fa8ae93
elferchando <efpa1998@hotmail.com>
2025-09-01 22:20:54 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
+import node from "@astrojs/node";
 
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "server",
+  adapter: node({ mode: "standalone" }),
   server: {
     host: "0.0.0.0",
-    port: Number(process.env.PORT) || 4321,
-  },
+    port: Number(process.env.PORT) || 4321
+  }
 });
